bea027b4cd45bb8490daeb4f2cc80d273fd051f4
najmusyathir <alsyathir@gmail.com>
2024-03-08 07:24:54 +0800

    Modify: Fix false import

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const { sendWebSocketMessage } = require('./webSocket');
+const { sendWebSocketMessage } = require('./WebSocket');
 
 /**
  * @swagger
